734f0d42f46a56d1a8db3bc1601bfc35203eb393
Breno Felix <brenodev.felix@gmail.com>
2022-11-19 16:45:43 -0300

    :wrench: chore:  instalação e conexão do banco de dados.

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 import express from "express"
 import routes from "./routes"
+import connectMongoDB from "./database/database"
 
 class App {
     constructor(){
         this.app = express()
 
+        this.database()
         this.middlewares()
         this.routes()
+    }
+
+    database(){
+        connectMongoDB()
     }
 
     middlewares(){
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
 import app from './app'
 
-app.listen(3002)
+const port = 3001
+
+app.listen(port, () => console.log(`server running on port ${port}`))
