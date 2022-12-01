e404149a8e28a28c7c53c9573f7158356ca8a406
Breno Felix <brenodev.felix@gmail.com>
2022-12-01 16:32:21 -0300

    :test_tube: test: ensure LoginRouter returns 400 if no email is provided

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,17 @@ module.exports = {
   env: {
     es2021: true,
     node: true,
+    'jest/globals': true
   },
-  extends: ["standard", "prettier"],
-  plugins: ["prettier"],
+  extends: ['standard', 'prettier'],
+  plugins: ['prettier', 'jest'],
   overrides: [],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
   rules: {
-    camelcase: "off",
-    "prettier/prettier": "error",
-  },
-};
+    camelcase: 'off',
+    'prettier/prettier': 'error'
+  }
+}
