dde8f260c61812987e1216c7b80fa519128e1875
Breno Felix <brenodev.felix@gmail.com>
2022-12-20 11:26:32 -0300

    :truck:	chore: Move and adapt envfile.js to src/main.

---
+++ src/main/envfile.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const dotenv = require('dotenv')
+
+dotenv.config({
+  path: path.join(
+    __dirname,
+    '..',
+    '..',
+    process.env.NODE_ENV === 'test' ? '.env.testing' : '.env'
+  )
+})
+
+module.exports = {
+  // production
+  dbUrl: process.env.DB_URL,
+  port: process.env.PORT,
+  // test
+  urlMongooseTest: process.env.MONGODB_DSN
+}
