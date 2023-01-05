4b9b7b6652256ceeb66db3dc8a2fa3dc4a2e4cdb
Breno Felix <brenodev.felix@gmail.com>
2023-01-05 14:56:20 -0300

    :test_tube: test: Ensure SignUpRouter returns 400 if no password is provided

--- src/presentation/routers/sign-up-router.spec.js
+++ src/presentation/routers/sign-up-router.spec.js
@@ -1,6 +1,6 @@
 class SignUpRouter {
   route(httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400
       }
@@ -22,6 +22,21 @@ describe('Sign Up Router', () => {
       body: {
         name: 'any_name',
         password: 'any_password',
+        repeatPassword: 'any_password',
+        admin: false
+      }
+    }
+    const httpResponse = await sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    // expect(httpResponse.body.error).toBe(new MissingParamError('email').message)
+  })
+
+  test('Should return 400 if no password is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
         repeatPassword: 'any_password',
         admin: false
       }
