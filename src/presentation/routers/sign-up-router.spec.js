60610330deaf58ca817016c9ac83d793f495d4f8
Breno Felix <brenodev.felix@gmail.com>
2023-01-05 14:48:34 -0300

    :test_tube: test: Ensure SignUpRouter returns 400 if no email is provided

---
+++ src/presentation/routers/sign-up-router.spec.js
@@ -0,0 +1,33 @@
+class SignUpRouter {
+  route(httpRequest) {
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400
+      }
+    }
+  }
+}
+
+const makeSut = () => {
+  const sut = new SignUpRouter()
+  return {
+    sut
+  }
+}
+
+describe('Sign Up Router', () => {
+  test('Should return 400 if no email is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        repeatPassword: 'any_password',
+        admin: false
+      }
+    }
+    const httpResponse = await sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    // expect(httpResponse.body.error).toBe(new MissingParamError('email').message)
+  })
+})
