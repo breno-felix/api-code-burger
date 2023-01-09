efb958542bf9f9a64ef45452ca3e05f02696c103
Breno Felix <brenodev.felix@gmail.com>
2023-01-09 12:50:43 -0300

    :recycle: refactor: improve LoginRouter separating the validation logic from the error handling logic

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -9,21 +9,28 @@ module.exports = class LoginRouter {
   async route(httpRequest) {
     try {
       const { email, password } = httpRequest.body
+
       if (!email) {
-        return HttpResponse.badRequest(new MissingParamError('email'))
+        throw new MissingParamError('email')
       }
       if (!this.emailValidator.isValid(email)) {
-        return HttpResponse.badRequest(new InvalidParamError('email'))
+        throw new InvalidParamError('email')
       }
       if (!password) {
-        return HttpResponse.badRequest(new MissingParamError('password'))
+        throw new MissingParamError('password')
       }
       const accessToken = await this.authUseCase.auth(email, password)
       if (!accessToken) {
         return HttpResponse.unauthorizedError()
       }
       return HttpResponse.ok({ accessToken })
-    } catch {
+    } catch (error) {
+      if (
+        error instanceof MissingParamError ||
+        error instanceof InvalidParamError
+      ) {
+        return HttpResponse.badRequest(error)
+      }
       return HttpResponse.serverError()
     }
   }
