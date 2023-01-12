5b49281c69740bd023830c8d8b8da084cfd7f2be
Breno Felix <brenodev.felix@gmail.com>
2023-01-12 17:29:15 -0300

    :truck: chore: Move SignUpUseCase to its own file

--- src/domain/usecases/sign-up-usecase.spec.js
+++ src/domain/usecases/sign-up-usecase.spec.js
@@ -1,44 +1,9 @@
+const SignUpUseCase = require('./sign-up-usecase')
 const {
   MissingParamServerError,
   RepeatedEmailError,
   RepeatPasswordError
 } = require('../../utils/errors')
-
-class SignUpUseCase {
-  constructor({
-    loadUserByEmailRepository,
-    encrypter,
-    createUserRepository
-  } = {}) {
-    this.loadUserByEmailRepository = loadUserByEmailRepository
-    this.encrypter = encrypter
-    this.createUserRepository = createUserRepository
-  }
-
-  async signUp(httpRequest) {
-    if (!httpRequest) {
-      throw new MissingParamServerError('httpRequest')
-    }
-
-    if (httpRequest.repeatPassword !== httpRequest.password) {
-      throw new RepeatPasswordError()
-    }
-
-    const user = await this.loadUserByEmailRepository.load(httpRequest.email)
-
-    if (user) {
-      throw new RepeatedEmailError()
-    }
-
-    const hashedPassword = await this.encrypter.hash(httpRequest.password)
-    await this.createUserRepository.create({
-      name: httpRequest.name,
-      email: httpRequest.email,
-      password: hashedPassword,
-      admin: httpRequest.admin
-    })
-  }
-}
 
 const makeSut = () => {
   const loadUserByEmailRepositorySpy = makeLoadUserByEmailRepository()
