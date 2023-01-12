19fb589991287f34f6ac0432617ea2bdf2208ed0
Breno Felix <brenodev.felix@gmail.com>
2023-01-11 22:28:08 -0300

    :test_tube: test: ensure SignUpUseCase call LoadUserByEmailRepository with correct email

--- src/domain/usecases/sign-up-usecase.spec.js
+++ src/domain/usecases/sign-up-usecase.spec.js
@@ -1,18 +1,52 @@
 const { MissingParamServerError } = require('../../utils/errors')
 
 class SignUpUseCase {
+  constructor({ loadUserByEmailRepository } = {}) {
+    this.loadUserByEmailRepository = loadUserByEmailRepository
+  }
+
   async signUp(httpRequest) {
     if (!httpRequest) {
       throw new MissingParamServerError('httpRequest')
     }
+    await this.loadUserByEmailRepository.load(httpRequest.email)
   }
 }
 
+const makeLoadUserByEmailRepository = () => {
+  class LoadUserByEmailRepositorySpy {
+    async load(email) {
+      this.email = email
+      return this.user
+    }
+  }
+  const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy()
+  loadUserByEmailRepositorySpy.user = {
+    id: 'any_id',
+    password: 'hashed_password'
+  }
+  return loadUserByEmailRepositorySpy
+}
+
+const makeLoadUserByEmailRepositoryWithError = () => {
+  class LoadUserByEmailRepositorySpy {
+    async load() {
+      throw new Error()
+    }
+  }
+  return new LoadUserByEmailRepositorySpy()
+}
+
 const makeSut = () => {
-  const sut = new SignUpUseCase()
+  const loadUserByEmailRepositorySpy = makeLoadUserByEmailRepository()
+
+  const sut = new SignUpUseCase({
+    loadUserByEmailRepository: loadUserByEmailRepositorySpy
+  })
 
   return {
-    sut
+    sut,
+    loadUserByEmailRepositorySpy
   }
 }
 
@@ -22,5 +56,20 @@ describe('Sign up UseCase', () => {
     expect(sut.signUp()).rejects.toThrow(
       new MissingParamServerError('httpRequest')
     )
+  })
+
+  test('Should call LoadUserByEmailRepository with correct email', async () => {
+    const { sut, loadUserByEmailRepositorySpy } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        repeatPassword: 'any_password',
+        admin: false
+      }
+    }
+    await sut.signUp(httpRequest.body)
+    expect(loadUserByEmailRepositorySpy.email).toBe(httpRequest.body.email)
   })
 })
