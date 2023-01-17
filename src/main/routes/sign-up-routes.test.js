02a12eaa753fe254ddae7547c236a0d4e421e6d8
Breno Felix <brenodev.felix@gmail.com>
2023-01-17 09:46:17 -0300

    :test_tube: test: Add sign up route integration test to ensure 400 response when any parameter is not provided

--- src/main/routes/sign-up-routes.test.js
+++ src/main/routes/sign-up-routes.test.js
@@ -4,7 +4,9 @@ const MongooseHelper = require('../../infra/helpers/mongoose-helper')
 const env = require('../config/envfile')
 const UserModel = require('../../infra/entities/UserModel')
 
-describe('Login Routes', () => {
+const requiredParams = ['name', 'email', 'password', 'repeatPassword']
+
+describe('Sign Up Routes', () => {
   beforeAll(async () => {
     await MongooseHelper.connect(env.urlMongooseTest)
   })
@@ -38,5 +40,19 @@ describe('Login Routes', () => {
     expect(users[0].name).toBe(userTest.name)
     expect(users[0].password).toEqual(expect.anything())
     expect(users[0].password).not.toBe(userTest.password)
+  })
+
+  requiredParams.forEach((param) => {
+    test(`Should return 400 if no ${param} is provided`, async () => {
+      const userTest = {
+        name: 'valid_name',
+        email: 'valid_email@mail.com',
+        password: 'valid_password',
+        repeatPassword: 'valid_password'
+      }
+      delete userTest[param]
+      const response = await request(app).post('/api/sign-up').send(userTest)
+      expect(response.status).toBe(400)
+    })
   })
 })
