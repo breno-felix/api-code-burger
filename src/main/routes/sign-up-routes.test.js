aeae91e773fe708939e0ea463ddcca70ba059cc5
Breno Felix <brenodev.felix@gmail.com>
2023-01-17 09:31:30 -0300

    :test_tube: test: Add sign up route integration test to ensure 201 response when valid data are provided

--- src/main/routes/sign-up-routes.test.js
+++ src/main/routes/sign-up-routes.test.js
@@ -18,14 +18,25 @@ describe('Login Routes', () => {
   })
 
   test('Should return 201 when valid data are provided', async () => {
-    await request(app)
-      .post('/api/sign-up')
-      .send({
-        name: 'valid_name',
-        email: 'valid_email@mail.com',
-        password: 'valid_password',
-        repeatPassword: 'valid_password'
-      })
-      .expect(201)
+    let users = await UserModel.find({})
+    expect(users.length).toBe(0)
+
+    const userTest = {
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password',
+      repeatPassword: 'valid_password'
+    }
+    const response = await request(app).post('/api/sign-up').send(userTest)
+    expect(response.status).toBe(201)
+
+    users = await UserModel.find({})
+    expect(users.length).toBe(1)
+    expect(users[0]._id).toEqual(expect.anything())
+    expect(users[0].admin).toBe(false)
+    expect(users[0].email).toBe(userTest.email)
+    expect(users[0].name).toBe(userTest.name)
+    expect(users[0].password).toEqual(expect.anything())
+    expect(users[0].password).not.toBe(userTest.password)
   })
 })
