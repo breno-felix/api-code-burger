e9a7b82c577cba584eba01579bd80bdfe2733aba
Breno Felix <brenodev.felix@gmail.com>
2023-01-18 18:00:32 -0300

    :truck: chore: Move NewCategoryRouter to its own file

--- src/presentation/routers/new-category-router.spec.js
+++ src/presentation/routers/new-category-router.spec.js
@@ -1,35 +1,4 @@
-const HttpResponse = require('../helpers/http-response')
-
-class NewCategoryRouter {
-  constructor({ newCategoryUseCase, objectShapeValidator } = {}) {
-    this.newCategoryUseCase = newCategoryUseCase
-    this.objectShapeValidator = objectShapeValidator
-  }
-
-  async route(httpRequest) {
-    try {
-      const requiredParams = ['name']
-      requiredParams.forEach((param) => {
-        if (!httpRequest.body[param]) {
-          throw new MissingParamError(param)
-        }
-      })
-      await this.objectShapeValidator.isValid(httpRequest.body)
-      await this.newCategoryUseCase.record(httpRequest.body)
-      return HttpResponse.created()
-    } catch (error) {
-      if (error instanceof RepeatedNameError) {
-        return HttpResponse.badRequest(new RepeatedNameError())
-      } else if (error instanceof InvalidParamError) {
-        return HttpResponse.badRequest(error)
-      } else if (error instanceof MissingParamError) {
-        return HttpResponse.badRequest(error)
-      }
-      return HttpResponse.serverError()
-    }
-  }
-}
-
+const NewCategoryRouter = require('./new-category-router')
 const { ServerError } = require('../errors')
 const {
   MissingParamError,
