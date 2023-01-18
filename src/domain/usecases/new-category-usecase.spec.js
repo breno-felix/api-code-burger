bef920a24030dff7fd6acdf6c8136d5d67e51a7e
Breno Felix <brenodev.felix@gmail.com>
2023-01-18 20:30:12 -0300

    :truck: chore: Move NewCategoryUseCase to its own file

--- src/domain/usecases/new-category-usecase.spec.js
+++ src/domain/usecases/new-category-usecase.spec.js
@@ -1,28 +1,4 @@
-class NewCategoryUseCase {
-  constructor({ loadCategoryByNameRepository, createCategoryRepository } = {}) {
-    this.loadCategoryByNameRepository = loadCategoryByNameRepository
-    this.createCategoryRepository = createCategoryRepository
-  }
-
-  async record(httpRequest) {
-    if (!httpRequest) {
-      throw new MissingParamServerError('httpRequest')
-    }
-
-    const category = await this.loadCategoryByNameRepository.load(
-      httpRequest.name
-    )
-
-    if (category) {
-      throw new RepeatedNameError()
-    }
-
-    await this.createCategoryRepository.create({
-      name: httpRequest.name
-    })
-  }
-}
-
+const NewCategoryUseCase = require('./new-category-usecase')
 const {
   MissingParamServerError,
   RepeatedNameError
