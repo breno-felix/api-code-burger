78a2b7dd422e662c6e64c167f988bf8fd0b6a70d
Breno Felix <brenodev.felix@gmail.com>
2023-01-24 18:27:58 -0300

    :test_tube: test: Add new category route integration test to ensure require authorization.

--- src/main/routes/new-category-routes.js
+++ src/main/routes/new-category-routes.js
@@ -1,7 +1,8 @@
 const { adapt } = require('../adapters/express-router-adapter')
 const NewCategoryRouterComposer = require('../composers/new-category-router-composer')
+const authMiddleware = require('../middlewares/auth')
 
 module.exports = (router) => {
   const newCategoryRouter = NewCategoryRouterComposer.compose()
-  router.post('/new-category', adapt(newCategoryRouter))
+  router.post('/new-category', authMiddleware, adapt(newCategoryRouter))
 }
--- src/main/routes/new-category-routes.test.js
+++ src/main/routes/new-category-routes.test.js
@@ -3,10 +3,30 @@ const app = require('../config/app')
 const MongooseHelper = require('../../infra/helpers/mongoose-helper')
 const env = require('../config/envfile')
 const CategoryModel = require('../../infra/entities/CategoryModel')
+const UserModel = require('../../infra/entities/UserModel')
+const bcrypt = require('bcrypt')
+let accessToken
+
+const loginUser = async (email, password) => {
+  const response = await request(app)
+    .post('/api/login')
+    .send({ email, password })
+  expect(response.status).toBe(200)
+
+  return response.body.accessToken
+}
 
 describe('New Category Routes', () => {
   beforeAll(async () => {
     await MongooseHelper.connect(env.urlMongooseTest)
+    await UserModel.deleteMany()
+    const fakeUser = new UserModel({
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: bcrypt.hashSync('hashed_password', 10)
+    })
+    await fakeUser.save()
+    accessToken = await loginUser('valid_email@mail.com', 'hashed_password')
   })
 
   beforeEach(async () => {
@@ -14,10 +34,15 @@ describe('New Category Routes', () => {
   })
 
   afterAll(async () => {
+    await UserModel.deleteMany()
     await MongooseHelper.disconnect()
   })
 
   const requiredParams = ['name']
+
+  test('Should require authorization', async () => {
+    await request(app).post('/api/new-category').expect(401)
+  })
 
   test('Should return 201 when valid data are provided', async () => {
     let categories = await CategoryModel.find({})
@@ -26,8 +51,10 @@ describe('New Category Routes', () => {
     const categoryTest = {
       name: 'valid_name'
     }
+
     const response = await request(app)
       .post('/api/new-category')
+      .auth(accessToken, { type: 'bearer' })
       .send(categoryTest)
     expect(response.status).toBe(201)
 
@@ -45,6 +72,7 @@ describe('New Category Routes', () => {
       delete categoryTest[param]
       const response = await request(app)
         .post('/api/new-category')
+        .auth(accessToken, { type: 'bearer' })
         .send(categoryTest)
       expect(response.status).toBe(400)
     })
@@ -54,13 +82,18 @@ describe('New Category Routes', () => {
     const category = {
       name: 'any_name'
     }
-    await request(app).post('/api/new-category').send(category)
+    await request(app)
+      .post('/api/new-category')
+      .auth(accessToken, { type: 'bearer' })
+      .send(category)
+      .expect(201)
 
     const categoryTest = {
       name: 'any_name'
     }
     const response = await request(app)
       .post('/api/new-category')
+      .auth(accessToken, { type: 'bearer' })
       .send(categoryTest)
     expect(response.status).toBe(400)
   })
