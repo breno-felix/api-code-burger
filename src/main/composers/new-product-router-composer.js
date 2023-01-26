ec7c3a5c4f61f5b1d3c3e42a232ceb894de6492c
Breno Felix <brenodev.felix@gmail.com>
2023-01-26 19:11:27 -0300

    :construction: Add newProductUseCase to NewProductRouterComposer.

--- src/main/composers/new-product-router-composer.js
+++ src/main/composers/new-product-router-composer.js
@@ -1,18 +1,28 @@
 const NewProductRouter = require('../../presentation/routers/new-product-router')
 const ObjectShapeValidator = require('../../utils/helpers/object-shape-validator')
 const CreateProductRepository = require('../../infra/repositories/product/create-product-repository')
+const NewProductUseCase = require('../../domain/usecases/new-product-usecase')
+const LoadCategoryByIdRepository = require('../../infra/repositories/category/load-category-by-id-repository')
 const productModel = require('../../infra/entities/ProductModel')
+const categoryModel = require('../../infra/entities/CategoryModel')
 const yupProductSchema = require('../../utils/helpers/yup-productSchema')
 
 module.exports = class NewProductRouterComposer {
   static compose() {
+    const createProductRepository = new CreateProductRepository(productModel)
+    const loadCategoryByIdRepository = new LoadCategoryByIdRepository(
+      categoryModel
+    )
+    const newProductUseCase = new NewProductUseCase({
+      loadCategoryByIdRepository,
+      createProductRepository
+    })
     const objectShapeValidator = new ObjectShapeValidator({
       yupSchema: yupProductSchema
     })
-    const createProductRepository = new CreateProductRepository(productModel)
     return new NewProductRouter({
       objectShapeValidator,
-      createProductRepository
+      newProductUseCase
     })
   }
 }
