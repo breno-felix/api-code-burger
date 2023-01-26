24f05e4fcf244d485a292f8402344a60eb6e9f72
Breno Felix <brenodev.felix@gmail.com>
2023-01-26 17:46:11 -0300

    :test_tube: test: Ensure NewProductUseCase calls CreateProductRepository with correct values

--- src/domain/usecases/new-product-usecase.spec.js
+++ src/domain/usecases/new-product-usecase.spec.js
@@ -1,6 +1,7 @@
 class NewProductUseCase {
-  constructor({ loadCategoryByIdRepository } = {}) {
+  constructor({ loadCategoryByIdRepository, createProductRepository } = {}) {
     this.loadCategoryByIdRepository = loadCategoryByIdRepository
+    this.createProductRepository = createProductRepository
   }
 
   async record(httpRequest) {
@@ -15,6 +16,8 @@ class NewProductUseCase {
     if (!category) {
       throw new CategoryNotCreatedError()
     }
+
+    await this.createProductRepository.create(httpRequest)
   }
 }
 
@@ -25,14 +28,17 @@ const {
 
 const makeSut = () => {
   const loadCategoryByIdRepositorySpy = makeLoadCategoryByIdRepository()
+  const createProductRepositorySpy = makeCreateProductRepository()
 
   const sut = new NewProductUseCase({
-    loadCategoryByIdRepository: loadCategoryByIdRepositorySpy
+    loadCategoryByIdRepository: loadCategoryByIdRepositorySpy,
+    createProductRepository: createProductRepositorySpy
   })
 
   return {
     sut,
-    loadCategoryByIdRepositorySpy
+    loadCategoryByIdRepositorySpy,
+    createProductRepositorySpy
   }
 }
 
@@ -48,6 +54,19 @@ const makeLoadCategoryByIdRepository = () => {
     id: 'valid_id'
   }
   return loadCategoryByIdRepositorySpy
+}
+
+const makeCreateProductRepository = () => {
+  class CreateProductRepositorySpy {
+    async create({ name, price, category_id, imagePath }) {
+      this.name = name
+      this.price = price
+      this.category_id = category_id
+      this.imagePath = imagePath
+    }
+  }
+  const createProductRepositorySpy = new CreateProductRepositorySpy()
+  return createProductRepositorySpy
 }
 
 describe('New Product UseCase', () => {
@@ -87,5 +106,21 @@ describe('New Product UseCase', () => {
     expect(sut.record(httpRequest)).rejects.toThrow(
       new CategoryNotCreatedError()
     )
+  })
+
+  test('Should call CreateProductRepository with correct values', async () => {
+    const { sut, createProductRepositorySpy } = makeSut()
+
+    const createSpy = jest.spyOn(createProductRepositorySpy, 'create')
+
+    const httpRequest = {
+      name: 'valid_name',
+      price: 10.01,
+      category_id: 'invalid_category_id',
+      imagePath: 'valid_name'
+    }
+
+    await sut.record(httpRequest)
+    expect(createSpy).toHaveBeenCalledWith(httpRequest)
   })
 })
