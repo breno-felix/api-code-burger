073a1a759f1d8adda034cd49aa5c5e314496a82e
Breno Felix <brenodev.felix@gmail.com>
2023-01-27 15:49:49 -0300

    :truck: chore: Move NewOrderRouter to its own file

---
+++ src/presentation/routers/new-order-router.js
@@ -0,0 +1,36 @@
+const HttpResponse = require('../helpers/http-response')
+const {
+  MissingParamError,
+  InvalidParamError,
+  ProductNotCreatedError
+} = require('../../utils/errors')
+
+module.exports = class NewOrderRouter {
+  constructor({ objectShapeValidator, newOrderUseCase } = {}) {
+    this.objectShapeValidator = objectShapeValidator
+    this.newOrderUseCase = newOrderUseCase
+  }
+
+  async route(httpRequest) {
+    try {
+      const requiredParams = ['products']
+      requiredParams.forEach((param) => {
+        if (!httpRequest.body[param]) {
+          throw new MissingParamError(param)
+        }
+      })
+      await this.objectShapeValidator.isValid(httpRequest.body)
+      await this.newOrderUseCase.record(httpRequest.body)
+      return HttpResponse.created()
+    } catch (error) {
+      if (error instanceof ProductNotCreatedError) {
+        return HttpResponse.badRequest(new ProductNotCreatedError())
+      } else if (error instanceof InvalidParamError) {
+        return HttpResponse.badRequest(error)
+      } else if (error instanceof MissingParamError) {
+        return HttpResponse.badRequest(error)
+      }
+      return HttpResponse.serverError()
+    }
+  }
+}
--- src/presentation/routers/new-order-router.spec.js
+++ src/presentation/routers/new-order-router.spec.js
@@ -1,35 +1,4 @@
-const HttpResponse = require('../helpers/http-response')
-
-class NewOrderRouter {
-  constructor({ objectShapeValidator, newOrderUseCase } = {}) {
-    this.objectShapeValidator = objectShapeValidator
-    this.newOrderUseCase = newOrderUseCase
-  }
-
-  async route(httpRequest) {
-    try {
-      const requiredParams = ['products']
-      requiredParams.forEach((param) => {
-        if (!httpRequest.body[param]) {
-          throw new MissingParamError(param)
-        }
-      })
-      await this.objectShapeValidator.isValid(httpRequest.body)
-      await this.newOrderUseCase.record(httpRequest.body)
-      return HttpResponse.created()
-    } catch (error) {
-      if (error instanceof ProductNotCreatedError) {
-        return HttpResponse.badRequest(new ProductNotCreatedError())
-      } else if (error instanceof InvalidParamError) {
-        return HttpResponse.badRequest(error)
-      } else if (error instanceof MissingParamError) {
-        return HttpResponse.badRequest(error)
-      }
-      return HttpResponse.serverError()
-    }
-  }
-}
-
+const NewOrderRouter = require('./new-order-router')
 const { ServerError } = require('../errors')
 const {
   MissingParamError,
