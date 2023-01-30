8bb60468a96cfc94e60146c55231d6cba69cb0e2
Breno Felix <brenodev.felix@gmail.com>
2023-01-30 09:30:30 -0300

    :test_tube: test: Create all index order route unit tests

---
+++ src/presentation/routers/index-order-router.js
@@ -0,0 +1,16 @@
+const HttpResponse = require('../helpers/http-response')
+
+module.exports = class IndexOrderRouter {
+  constructor({ loadAllOrderRepository } = {}) {
+    this.loadAllOrderRepository = loadAllOrderRepository
+  }
+
+  async route() {
+    try {
+      const orders = await this.loadAllOrderRepository.load()
+      return HttpResponse.ok(orders)
+    } catch (error) {
+      return HttpResponse.serverError()
+    }
+  }
+}
