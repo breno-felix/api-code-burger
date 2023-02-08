768c0105921a5d3b45eef187b61343d2aee3b7ae
Breno Felix <brenodev.felix@gmail.com>
2023-02-08 19:29:55 -0300

    :construction: Improve models adding virtuals and timestamps.

--- src/infra/entities/OrderModel.js
+++ src/infra/entities/OrderModel.js
@@ -1,27 +1,24 @@
 const MongooseHelper = require('../helpers/mongoose-helper')
 
-const orderSchema = MongooseHelper.newSchema(
-  {
-    user_id: {
-      type: MongooseHelper.getObjectId(),
-      ref: 'User',
-      required: true
-    },
-    products: [
-      {
-        product_id: {
-          type: MongooseHelper.getObjectId(),
-          ref: 'Product',
-          required: true
-        },
-        quantity: { type: Number, required: true, min: 0 }
-      }
-    ],
-    status: { type: String, required: true, default: 'Pedido realizado' }
+const orderSchema = MongooseHelper.newSchema({
+  user_id: {
+    type: MongooseHelper.getObjectId(),
+    ref: 'User',
+    required: true
   },
-  {
-    timestamps: true
-  }
-)
+  products: [
+    {
+      product_id: {
+        type: MongooseHelper.getObjectId(),
+        ref: 'Product',
+        required: true
+      },
+      quantity: { type: Number, required: true, min: 0 }
+    }
+  ],
+  status: { type: String, required: true, default: 'Pedido realizado' }
+})
+
+orderSchema.set('timestamps', true)
 
 module.exports = MongooseHelper.newModel('Order', orderSchema)
