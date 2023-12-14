139d47f29e9592883770e1583071732cafdccaea
Breno Felix <brenodev.felix@gmail.com>
2023-12-14 19:45:23 -0300

    :wrench:	chore: config cors.

--- src/main/config/app.js
+++ src/main/config/app.js
@@ -3,10 +3,11 @@ const express = require('express')
 const path = require('path')
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocs = require('../documentation/swagger.js')
+const cors = require('cors')
 class App {
   constructor() {
     this.app = express()
-
+    this.app.use(cors())
     this.middlewares()
     this.routes()
   }
